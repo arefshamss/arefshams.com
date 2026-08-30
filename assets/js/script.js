35b28e30a8c6b04fe5602db9cf92d7f265644b24
Aref Shamspour <sh.aref@ymail.com>
2026-08-30 22:29:16 +0330

    fix: force pdf download using fetch and blob

--- assets/js/script.js
+++ assets/js/script.js
@@ -470,6 +470,35 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 ///////////////////////////////////////////////////////////
+// Resume Download
+document
+  .getElementById("resume-download-btn")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const fileUrl = this.getAttribute("href");
+
+    fetch(fileUrl)
+      .then((response) => response.blob())
+      .then((blob) => {
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement("a");
+        a.style.display = "none";
+        a.href = url;
+        a.download = "Aref-Shamspour-Resume.pdf";
+        document.body.appendChild(a);
+
+        a.click();
+
+        window.URL.revokeObjectURL(url);
+        document.body.removeChild(a);
+      })
+      .catch((err) => {
+        window.open(fileUrl, "_blank");
+      });
+  });
+
+///////////////////////////////////////////////////////////
 // Contact Form
 const form = document.getElementById("contact-form");
 const result = document.getElementById("form-result");
